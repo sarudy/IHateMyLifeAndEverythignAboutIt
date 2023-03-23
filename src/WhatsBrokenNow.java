b98a5fe68faa7980283155b1a76f3cf5c788f8f6
Sylvia A Rudy <sylvia.rudy@martindale.com>
2023-03-23 18:41:45 -0400

    I got the stupid ENUM lowercase thing figured out.  Not why I'm here but what the heck

--- src/WhatsBrokenNow.java
+++ src/WhatsBrokenNow.java
@@ -1,5 +1,7 @@
+import model.RoomType;
+
 public class WhatsBrokenNow {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(RoomType.SINGLE);
     }
 }
--- src/model/RoomType.java
+++ src/model/RoomType.java
@@ -1,5 +1,19 @@
 package model;
 
 public enum RoomType {
-    SINGLE, DOUBLE
+    SINGLE("Single"),
+    DOUBLE("Double");
+
+    public final String label;
+
+    RoomType(String label) {
+        this.label = label;
+    }
+    public String getLabel() {
+        return label;
+    }
+    @Override
+    public String toString() {
+        return label;
+    }
 }
